78b35ea90c0454f0c5fb10f650f088ec6ae1b16d
sivaramgunabalan <93179953+sivaramgunabalan@users.noreply.github.com>
2022-05-30 21:24:41 +0530

    Add files via upload

---
+++ ConsoleApp1Tests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void BigNumberTest()
+        {
+            Program p = new Program();
+            Assert.IsTrue(p.BigNumber(102));
+        }
+    }
+}
